65e7df1d44a0d6fc0c17eb41ef1e9c6c65a78faa
jitendra9661 <jitendrakumarsuman9661@gmail.com>
2021-03-16 17:54:28 +0530

    first commit/upload

---
+++ src/main/java/com/example/MyFirstSpringBootProjectEclipse/MyFirstSpringBootProjectEclipseApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyFirstSpringBootProjectEclipse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringBootProjectEclipseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringBootProjectEclipseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyFirstSpringBootProjectEclipse/MyFirstSpringBootProjectEclipseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyFirstSpringBootProjectEclipse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringBootProjectEclipseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
